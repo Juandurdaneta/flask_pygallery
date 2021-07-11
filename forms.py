ff710707a8f94ba974e8be10df8d7217d670d9ae
Juan Urdaneta <juandurdanet@gmail.com>
2021-07-11 17:50:54 -0400

    Añadiendo formulario de registro

---
+++ forms.py
@@ -0,0 +1,10 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Nombre de Usuario', validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Correo Electronico', validators=[DataRequired(), Email()])
+    password = PasswordField('Contraseña', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirmar Contraseña', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Crear Cuenta')
--- pygallery.py
+++ pygallery.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '59eb5820bce3227f5308d8e82fa62e97'
 
 @app.route("/")
 @app.route("/home")
@@ -11,9 +13,15 @@ def home():
 def login():
     return render_template('login.html', title="login")
 
-@app.route("/register")
+@app.route("/register", methods=['GET', 'POST'])
 def register():
-    return render_template('register.html', title="register")
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f"Cuenta creada para {form.username.data}!", "success")
+        return redirect(url_for('home'))
+
+    return render_template('register.html', title="register", form=form)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
